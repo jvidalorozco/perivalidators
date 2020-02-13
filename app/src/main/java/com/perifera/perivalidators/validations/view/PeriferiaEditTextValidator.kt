dde1dcad788d417099172134fa25964a57a1b04a
Jorge V <jorgevidal1995@hotmail.com>
2020-02-12 21:56:29 -0500

    JV - Pruebas

--- app/src/main/java/com/perifera/perivalidators/validations/view/PeriferiaEditTextValidator.kt
+++ app/src/main/java/com/perifera/perivalidators/validations/view/PeriferiaEditTextValidator.kt
@@ -2,16 +2,19 @@
 
 package com.perifera.perivalidators.validations.view
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
+import com.perifera.perivalidators.utils.getLocale
 import com.perifera.perivalidators.validations.GeneralValidations
 
 import java.io.Console
+import java.text.NumberFormat
+import java.text.ParseException
 
 
-
-
-    /**
+/**
      * Retorna el valor del EditText
      * @version 1.0
      * @author Cristian S
@@ -49,5 +52,66 @@ import java.io.Console
                 .addErrorCallback {
                     onFailure.invoke(it)
                 }.check()
+    }
+
+
+
+    fun EditText.formatMoney(){
+
+        val mNumberFormat  = NumberFormat.getNumberInstance(getLocale(context))
+        val mCurrencyFormat = NumberFormat.getCurrencyInstance(getLocale(context))
+        var myFormattedPrice : String = ""
+
+        this.addTextChangedListener(object: TextWatcher {override fun afterTextChanged(s: Editable?) {
+
+        }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                try {
+
+                    // Use the number format for the locale.
+                    var mInputQuantity = mNumberFormat.parse(s?.toString()).toInt()
+
+                    // TODO: Set up the price and currency format.
+
+                    /*var deviceLocale = Locale.getDefault().country
+                    // If country code is France or Israel, calculate price
+                    // with exchange rate and change to the country's currency format.
+                    if (deviceLocale.equals("FR") || deviceLocale.equals("IL")) {
+                        if (deviceLocale.equals("FR")) {
+                            // Calculate mPrice in euros.
+                            mPrice *= mFrExchangeRate
+                        } else {
+                            // Calculate mPrice in new shekels.
+                            mPrice *= mIwExchangeRate;
+                        }
+                        // Use the user-chosen locale's currency format, which
+                        // is either France or Israel.
+                        myFormattedPrice = mCurrencyFormat.format(mInputQuantity)
+                    } else {
+                        // mPrice is the same (based on U.S. dollar).
+                        // Use the currency format for the U.S.
+                        mCurrencyFormat = NumberFormat.getCurrencyInstance(getLocale())
+                        myFormattedPrice = mCurrencyFormat.format(mInputQuantity)
+                    }*/
+
+                    myFormattedPrice = mCurrencyFormat.format(mInputQuantity)
+
+                    setText(myFormattedPrice)
+
+                } catch ( e: ParseException) {
+                    e.printStackTrace()
+
+                }
+            }
+
+        })
+
+
+
     }
 
